ffa6763496a1a0fb99247b789290fb58adbbb3ff
Darz_K <darrenkellydk@rogers.com>
2018-03-27 23:42:10 -0400

    initial commit

---
+++ Darren-Kelly-CV-2017.php
@@ -0,0 +1,510 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This is the CV webpage of Darren Kelly">
+
+<title>DARREN KELLY - CV</title>
+
+<link rel="stylesheet" type="text/css" href="css/Darz-cv.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!--change the fav-->
+<link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+<link rel="shortcut icon" href="faviconSub.ico" type="image/x-icon" /> 
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.2manyartistz.com/darrenkelly-cv.2manyartistz.com/Darren-Kelly-CV-April-2016.html"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik-analytix.2manyartistz.com/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 4]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik-analytix.2manyartistz.com/piwik/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
+
+</head>
+<body>
+<br>
+
+	<div class="wrapper">
+	<br>
+	<div >
+		<h1 class="slideLeft">DARREN KELLY</h1>
+	</div>
+	<!--contakt info-->
+	<div class="contact fadeInBlock">  
+		
+			<h3>Email:<a href="mailto:darrenkellydk@rogers.com" title="My Email address">darrenkellydk@rogers.com </a>
+			   <br><a href="http://ie.linkedin.com/in/darrenkellydk" target="_blank" title="My Linked In Profile">Linked in: Darren kelly</a>
+			                
+			    <br>PH: 647 380 4634</h3>
+	</div>
+			
+				<!--</div>-->
+		
+				<br>
+		
+		
+		<div class="container">
+		
+			
+					<!--PERSONAL PROFILE-->
+					
+			<div class="main">
+					<h2 class="slideRight">Personal Profile</h2>
+						<div class="fadeInBlock">
+							<h3 >
+								I am a creative and enthusiastic Web Developer with a strong interest in Design, 
+								as well as Development. 
+								I am as comfortable working as part of a team as I am working on my own initiative. 
+								I am looking for career and learning opportunities to gain experience and build 
+								on my existing knowledge.
+						    </h3>
+						</div><!--end fadeinblock-->
+						
+				<!--WORK EXPERIENCE-->
+						
+			<h2 class="slide">Work Experience</h2>
+			<div class="fadeInBlock">
+				
+			<h3><a href="http://2manyartistz.com/kerz.ie/index.html" target="_blank" title="My 2nd Website">Kerz.ie </a>
+					 
+					<br>Web Design / Web Development   <span>(11/2015 – 
+					<?php echo date('m/Y)');  ?> </span></h3>                                                                                                         
+					<ul>
+				 <h3><li>Created and Maintaining this website</li>
+				 	
+					</ul>
+
+				</div><!--end fadeinblock-->
+				<div class="fadeInBlock">
+					<h3><a href="http://www.2manyartistz.com" target="_blank" title="My Website">2manyartistz.com </a>
+					 
+					<br>Artist / Web Design / Web Development  <span>(09/2010 - <?php echo date('m/Y)');  ?></span></h3>                                                                                                         
+					<ul>
+				 <h3><li>Created and Maintaining this website</li>
+				 	 <li><a href="http://www.2manyartistz.com/New-Gallery-Page-1.html" target="_blank" title="My Art Work">Created Art Work on this website</li></a>
+					   <li>Distributing this Artwork</li>
+						</h3>
+					</ul>
+			</div><!--end fadeinblock-->
+				
+			<div class="fadeInBlock">
+					<h3>CPL Entertainment, Glasgow, G2, Scotland 
+						<span>(07/2004 - 09/2010) </span> 
+						               
+					<br>Light Jockey (Technician)</h3>
+			
+					<ul>
+				<h3><li>Programming of lighting desk</li>
+					 <li>Live co-ordination of lights with music</li> 
+					  <li>Lighting maintenance and troubleshooting </li></h3>
+					</ul>
+				</div><!--end fade in block-->
+
+
+				<!-- <div class="fadeInBlock">
+					<h3>Alpha Office Furniture, Hibernian Ind., Est., Dublin 24 
+						<span>(08/2000 – 12/2002)</span> 
+						               
+					<br>Assembler</h3>
+							
+					<ul>
+				<h3><li>Assembly of various different models of office furniture </li></h3>
+					</ul>
+				</div>end fade in block
+				
+				<div class="fadeInBlock">			
+					<h3>A.P.W, Airton Road, Tallaght, Dublin 24  
+					                   <span>(06/2000 - 08/2000)</span>                 
+					   <br>Electrical Assembler</h3>
+					<ul>
+					<h3><li>Assembly of mainframe server units  </li>	
+						<li>Trained new Employees</li></h3>
+					</ul>
+					</div> --><!--end fade in block-->	
+			
+				<!-- End WORK EXPERIENCE-->
+
+						
+						
+						
+					<!--KEY SKILLZ-->
+						
+					<h2 class="slideRight">Key Skills</h2>
+				<div class="fadeInBlock">
+					<ul>
+						<li><h3>Motivated and Enthusiastic – Designed and Developed Live websites and Online Portfolio CV/Résumé, and currently 
+						developing Online Portfolio Website</h3></li>
+						   <li><h3>Creative with strong technical problem solving ability – Have achieved numerous Certificates for Web Development and Internet related studies</h3></li>
+					</ul>
+				
+					
+				</div>	<!-- /KEY SKILLZ--><!--end fadeinblock-->
+				
+				<!--KEY Achievements-->
+					
+				<h2 class="slide">Key Achievements</h2>
+				 <div class="fadeInBlock">
+					<ul>
+						<li><h3>Achieved City and Guilds Diploma for Web Development</h3></li>
+					    <li><h3>Achieved Adobe InDesign, Adobe Illustrator and Adobe Photoshop ACA Certificates</h3></li>
+					    <li><h3>Achieved three certificates that make up the main certificate of Internet Webmaster</h3></li>
+					    <li><h3>Experience with HTML,CSS, JavaScript and jQuery, PHP, JAVA, WordPress and Bootstrap </h3></li>
+
+					</ul>
+				</div><!--end fadeinblock-->
+			<!--/ Key Achievements-->
+					
+			
+				<!--EDUCATION AND TRAINING-->
+					
+		<h2 class="slide">Education and Training</h2>
+		<!--fade this content in on load-->
+		<div class="fadeInBlock">
+
+		 
+			
+<h3> eCollege Online <span>(06/2016 - 01/2017)</span>
+			 <ul> <li>Adobe Photoshop ACA Certified</li></ul></h3> 
+
+			<h3> DPP Skillnet Desktop Publishing <span>(08/2015 - 02/2016)</span>
+			  <br>New Horizons, Strand street, Dublin 1</h3>
+			
+			
+			<!--start Indy kode here-->
+			
+			<ul>
+				<!--indesign modal-->
+				<li>
+					
+				<h3>Adobe InDesign
+					
+					<a href="#openModal20" 
+		 	     title="Click here to view: Adobe InDesign ACA Certificate">ACA Certified</a> </h3>
+					
+					
+			   <div id="openModal20" class="modalDialog">
+					<div><h3>
+						<a href="#close" title="Close" class="close">X</a>
+						<a href="assetz/largeCertz/InDesign-ACA-Cert.jpg" target="_blank" class="noUnd"> 	
+						<h3>Click to view PDF version of this Cert. in new tab</a>
+						<img  class="fitImage" src="assetz/Indesign-aca-cert.jpg" alt="Adobe InDesign ACA Certificate">
+						<!--open Indesign example in new tab-->
+						<a href="assetz/largeCertz/Hegelian-dialectic-March02016-book-cover-Large.jpg" target="_blank" class="noUnd"> 	
+						Click here to view an example of my InDesign work in a new tab window
+						</a>
+						</h3>
+					</div><!--/modal div-->
+							
+			  </div><!--end open indesign modal-->
+			</li>		
+					
+			<!--illustrator modal-->
+			<li>
+				<h3>Adobe Illustrator
+				<a href="#openModal21" 
+		 	     title="Click here to view: Adobe Illustrator ACA Certificate details">ACA Certified</a> </h3>
+				
+			
+			   <div id="openModal21" class="modalDialog">
+					<div>
+						<a href="#close" title="Close" class="close">X</a>
+						<a href="assetz/Illustrator-cert 001.jpg" target="_blank" class="noUnd"> 	
+						<h3>Click to view a Large version of this Cert. in new tab</a>
+						<img  class="fitImage" src="assetz/Illustrator-cert 001.jpg" alt="Adobe InDesign ACA Certificate"><!--put in illy cert-->
+						
+					<a href="assetz/Illustrator-example-1.jpg" target="_blank" class="noUnd" >	
+					<h3>Click to view a sample of work in a new tab</h3></a>
+						
+					</div><!--/modal div-->
+			  </div><!--end open illustrator modal-->
+			</li>		
+						
+		</ul>
+
+		<!-- new part - Online Training Lynda etc.. -->
+
+		<a href="http://ie.linkedin.com/in/darrenkellydk#experience"
+		target="_blank" title="These Certificates can be viewed on my full LinkedIn profile under JavaScript, CSS & Web Design"><h3>Online Training (Lynda.com)</a><span>(10/2014 – 05/2015) </span></h3>
+			  
+
+		<div class="fadeInBlock">
+					<ul>
+				<li><h3>JQuery Essential Training Certificate</h3></li>
+					    <li><h3>JavaScript Essential Training Certificate</h3></li>
+					    <li><h3>JavaScript for Web Designers Certificate </h3></li>
+					    <li><h3>JavaScript &amp; AJAX Certificate</h3></li>
+						<li><h3>CSS Transitions &amp; Transforms Certificate</h3></li>
+					
+					</ul>
+				</div>
+
+		<!--#background-experience-- /new part - Online Training Lynda etc.. -->
+	
+	</div><!--end fade in block-->
+	<!--/desktop publishing-->
+					
+			<h3> eCollege Online - <a href="#openModal" 
+			
+			 	title="Click here to view: Certified Internet Webmaster Certificate">Certified Internet Webmaster</a> foundations</h3>
+			<div id="openModal" class="modalDialog">
+				<div>
+					<a href="#close" title="Close" class="close">X</a>
+				<a href="assetz/largeCertz/Web-Foundations-Associate-Certificate.pdf" target="_blank" class="noUnd">	
+				<h3>Click to view PDF version of this Cert. in a new tab</h3></a>
+					<img  class="fitImage" src="assetz/web-foundationz-cert.jpg" / alt="Web foundations Cert">
+				This is a certificate achieved for passing 3 individual modules
+				</div>
+			</div>
+
+		<div class="fadeInBlock">			
+			<div class="">
+				<ul>  
+				  <li>
+					<h3><a href="#openModal2" title="Click here for: Network Technology Associate Certificate">
+					Network Technology Associate Certificate</a>
+						
+					   <div id="openModal2" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+							<a href="assetz/largeCertz/N.T.A-Certificate.pdf" target="_blank" class="noUnd"> 	
+							<h3>Click to view PDF version of this Cert. in new tab</h3></a>	
+							<img  class="fitImage" src="assetz/ciw-nta-cert.jpg"  alt="Network Technology Associate Certificate">
+							The 3rd module &amp; Certificate, part of the Certified Internet Webmaster foundations Course
+						</div><!--/modal div-->
+							
+			  </div><!--end open modal-->
+						
+					<span>(09/2014 - 10/2014)</span></h3>
+			  	  </li> 
+			  	  
+			  	  <!--sda cert - fix datez-->
+			  	  <li>
+					<h3><a href="#openModal3" title="Click here for: Site Development Associate Certificate">
+						Site Development Associate Certificate</a>
+						
+					   <div id="openModal3" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+							<a href="assetz/largeCertz/ciw-sda-cert.jpg" target="_blank" class="noUnd"> 	
+							<h3>Click to view larger version of this Cert. in new tab</h3></a>
+							<img  class="fitImage" src="assetz/ciw-sda-cert.jpg" / alt="Click here for: Site Development Associate Certificate">
+							The 2nd module &amp; Certificate, part of the Certified Internet Webmaster foundations Course
+							</div><!--/modal div-->
+							
+					  </div>
+						
+					<span>(06/2014 - 09/2014)</span></h3> 
+			  	  </li> 
+			  	  <!--i ba cert - fix datez-->
+			  	  <li>
+					<h3><a href="#openModal4" title="Click here for: Internet Business Associate Certificate">
+						Internet Business Associate Certificate</a>
+						
+					   <div id="openModal4" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+							<a href="assetz/largeCertz/ciw-iba-cert.jpg" target="_blank" class="noUnd"> 	
+							<h3>Click to view larger version of this Cert. in new tab</h3></a>
+							<img  class="fitImage" src="assetz/ciw-iba-cert.jpg" / alt="Click here for: Internet Business Associate Certificate">
+							The 1st module &amp; Certificate, part of the Certified Internet Webmaster foundations Course
+							</div><!--/modal div-->
+							
+					  </div>
+						
+					<span>(02/2014 - 05/2014)</span></h3> 
+			  	  </li> 
+					  
+			  	</ul>
+				
+				</div><!--end certz div-->
+			</div><!-- end fadeInBlock-->
+		
+		<div class="fadeInBlock">
+				<h3>College of Computer Training (CCT)
+				
+				 <span>(02/2013 - 06/2013)</span>   
+				 <br>30-34 Westmoreland St., Dublin 2                                                   
+			 	 <br>ICT Technologies Programme City and Guilds 	
+													
+			 
+			<!--City and Guilz diploma-->
+			
+			<a href="#openModal5" title="Click here for: City and Guilds Diploma for Web Development and Database Maintainance">
+						Diploma</a>
+		<div class="fadeInBlock">				
+					   <div id="openModal5" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+							<a href="assetz/largeCertz/web-diploma.jpg" target="_blank" class="noUnd"> 	
+				<h3>Click to view a larger version of this Cert. in new tab</h3></a>	
+							<img  class="fitImage" src="assetz/web-diploma.jpg" / alt="Click here for: City and Guilds Diploma for Web Development and Database Maintainance">
+							</div>
+						</div>	
+					  </div>
+			
+			in
+			<!--systemz and principlez cert-->
+				<a href="#openModal6" title="Click here for: Unit Credits for Web Development and Database Maintainance">
+						Systems 
+				&amp; Principles</a></h3>
+						
+					   <div id="openModal6" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+							<a href="assetz/largeCertz/web-diploma-creditz.jpg" target="_blank" class="noUnd"> 	
+				<h3>Click to view PDF version of this Cert. in new tab</h3></a>	
+							<img  class="fitImage" src="assetz/web-diploma-creditz.jpg" / alt="Click here for: Unit Credits for Web Development and Database Maintainance">
+							</div>
+							
+					  </div>
+			
+		
+			<ul> 
+			 <h3><li>Web Development 
+			  <li>Database software (MTA Cert.)</li>
+			   <li>ICT System and Data security </li>
+				<li>Advanced Data representation and manipulation </li>
+				 <li>ICT Customer Support </li>
+				<li>Advanced Web Design </li>
+			   <li>Object Oriented Programming (JAVA)</li></h3>
+			</ul>
+		</div><!--end fade-in div-->
+		
+		<div class="fadeInBlock">	
+			<h3>Springboard Opportunities Ltd. (N.D.P) 
+			<span>(05/2003 - 05/2004)</span>                  
+			<br>K.E.P, Tallaght, Dublin 24  (
+			
+			
+			<!--Toronto cert-->
+				<a href="#openModal7" title="Click here to view Certificate for work abroad in Toronto">
+						Work experience in Toronto</a>)</h3>
+						
+					   <div id="openModal7" class="modalDialog">
+							<div>
+							<a href="#close" title="Close" class="close">X</a>
+				<h4>Achieved this Certificate for working abroad in Toronto</h4>
+							<a href="assetz/largeCertz/springboard-toronto.jpg" target="_blank" class="noUnd"> 	
+				<h4>Click to view a larger version of this Cert. in new tab</h4></a>
+				<img  class="fitImage" src="assetz/springboard-toronto.jpg" / alt="Click here for: Certificate for work abroad">
+							
+							</div>
+							
+					  </div>
+			
+			<ul> 
+			 <h3><li>E.C.D.L and PC Maintenance</li> 
+			  <li>Microsoft Applications</li></h3>
+			</ul>
+		
+			<h3>K.C.S Community School, Tallaght, Dublin 24  
+			<span>(09/1994 - 05/1999)</span></h3>
+			
+			
+			<ul>                                        
+			 <h3><li>High School Diploma (1999)</li>
+			  
+			</ul>
+			
+		</div><!--end fade-in block-->	
+			
+			<!--Additional Information-->
+															   
+			<h2 class="slide">Additional Information</h2>
+		<div class="fadeInBlock">	
+			<ul>
+			 <h3><li>Completed Emergency First Aid Course</li>
+			  <li>Customer Service and Phone Techniques Course</li></h3>
+			</ul>
+		</div><!--end fade in onscroll-->
+			<!--Interests-->
+		
+			<h2 class="slide">Interests</h2>
+		<div class="fadeInBlock">
+			<ul>
+			<h3><li>Music - I create music, and listens to a lot of different styles.</li>
+			<li>Art - I create Original artwork.</li>
+			<li>Exercise - I enjoy cycling and walking regularly.</li></h3>
+			</ul>
+			
+		  </div><!--end fade in onscroll-->
+			
+
+
+
+
+		</div><!--end main-->
+
+	<!--footer-->
+	
+	<footer style="text-align:center; opacity: 0.5">
+			<h5>&copy; Darren Kelly - 2016 - <?php echo date('Y');  ?></h5>
+	</footer>
+	
+
+	 </div><!--end container-->
+</div> <!--end wrapper-->
+	
+<br>
+
+
+<!--animation jQuery-->
+<script>
+$(function() {
+	
+	$(window).bind("load scroll", function(){
+	
+        $('.fadeInBlock').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+           /* Adjust for delay or content starts fading a bit before reached */
+            bottom_of_window = bottom_of_window + 200;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},2800);
+                    
+            }
+        }); 
+        
+        /*headingz animate function*/
+          $('.slide').each( function(o){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+           /* Adjust for delay or content starts fading a bit before reached */
+            bottom_of_window = bottom_of_window + 100;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).addClass('slideRight');
+                    
+            }
+        }); 
+    
+    });
+});
+
+</script>
+
+</body>
+
+</html>
